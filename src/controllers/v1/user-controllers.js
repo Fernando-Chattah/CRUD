3fbe6390e0c2830ad69739aa773adc9b8d34c52a
Chattah <A125962@bf.arg.igrupobbva>
2021-05-10 19:21:08 -0300

    Actualizacion version bcryptjs

--- src/controllers/v1/user-controllers.js
+++ src/controllers/v1/user-controllers.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../../mongo/module/user');
 const Product = require('../../mongo/module/product');
 const jwt = require('jsonwebtoken');
